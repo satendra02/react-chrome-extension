5ff9187bc26e1466a8638f9fa8460a0f8b88bd82
Swizec Teller <swizec@swizec.com>
2019-07-12 22:42:57 -0700

    Hello world chrome extension with react

--- src/content.js
+++ src/content.js
@@ -4,24 +4,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./content.css";
 
-const app = document.createElement("div");
-app.id = "my-extension-root";
+const Giphys = () => {
+    return "HELOO WORLD";
+};
 
-document.body.appendChild(app);
-ReactDOM.render(<Main />, app);
+for (const node of document.getElementsByTagName("textarea")) {
+    const div = document.createElement("div");
+    node.parentNode.insertBefore(div, node);
 
-app.style.display = "none";
-
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.message === "clicked_browser_action") {
-        toggle();
-    }
-});
-
-function toggle() {
-    if (app.style.display === "none") {
-        app.style.display = "block";
-    } else {
-        app.style.display = "none";
-    }
+    ReactDOM.render(<Giphys />, div);
 }
