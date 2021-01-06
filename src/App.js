2a2cf72eee13a7988af72d7f1a3559e2da63806e
Emerson Hum <emerson.hum@affirm.com>
2021-01-05 22:51:25 -0800

    Add route-lite and affirm logo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 /*global chrome*/
 
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Router, { Link, goBack } from 'route-lite';
+import logo from './affirm-logo.svg';
 import './App.css';
+
+const Page = ({ text }) => {
+  return (
+    <div>
+      <p onClick={() => goBack()}>{text}</p>
+    </div>
+  )
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          {this.props.isExt ? 
-            <img src={chrome.runtime.getURL("static/media/logo.svg")} className="App-logo" alt="logo" />
-          :
-            <img src={logo} className="App-logo" alt="logo" />
-          }
-
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            {this.props.isExt ? 
+              <img src={chrome.runtime.getURL("static/media/affirm-logo.svg")} className="App-logo" alt="logo" />
+            :
+              <img src={logo} className="App-logo" alt="logo" />
+            }
+  
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+        <Link component={Page} componentProps={{text: 'help'}}>Go to Page</Link>
+      </Router>
     );
   }
 }
